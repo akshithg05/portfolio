af00536818900e38dbffbe3b866ce06a403ca8a3
Akshith G <akshithg01@gmail.com>
2025-12-14 12:58:13 +0530

    Updates

---
+++ precache-manifest.5b2382a1d564e2d60a1178368d044d64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4130dc3a03ea0e71c294a06a4f58f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "557303395a29332f1458",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "18b29b3e9868ecf61b46",
+    "url": "/portfolio/static/css/main.293f4549.chunk.css"
+  },
+  {
+    "revision": "557303395a29332f1458",
+    "url": "/portfolio/static/js/2.ee79b14f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.ee79b14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b29b3e9868ecf61b46",
+    "url": "/portfolio/static/js/main.d6447c92.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
